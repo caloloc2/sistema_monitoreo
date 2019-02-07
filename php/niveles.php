caab755692327c4b2bb330be51760bec87239b0f
caloloc2 <calolomino@gmail.com>
2019-02-07 17:06:25 -0500

    actualiza

---
+++ php/niveles.php
@@ -0,0 +1,28 @@
+<?php 
+
+require 'meta.php';
+
+$consulta = Meta::Consulta_Unico("SELECT * FROM tanques ORDER BY id_datos DESC LIMIT 1");
+$respuesta = "";
+
+if ($consulta['id_datos']!=''){    
+    if ($consulta['tanque1_nivel']<30){
+        $respuesta .= "0-";
+    }elseif ($consulta['tanque1_nivel']>80) {
+        $respuesta .= "1-";
+    }
+
+    if ($consulta['tanque2_nivel']<30){
+        $respuesta .= "0-";
+    }elseif ($consulta['tanque2_nivel']>80) {
+        $respuesta .= "1-";
+    }
+
+    if ($consulta['tanque3_nivel']<30){
+        $respuesta .= "0";
+    }elseif ($consulta['tanque3_nivel']>80) {
+        $respuesta .= "1";
+    }
+}
+
+echo  $respuesta;
