d06477f1bcaa39fb4b505f073051eb57b1fb0db6
caloloc2 <calolomino@gmail.com>
2019-02-07 17:15:05 -0500

    actualiza

--- php/niveles.php
+++ php/niveles.php
@@ -6,22 +6,28 @@ $consulta = Meta::Consulta_Unico("SELECT * FROM tanques ORDER BY id_datos DESC L
 $respuesta = "";
 
 if ($consulta['id_datos']!=''){    
-    if ($consulta['tanque1_nivel']<30){
+    if ($consulta['tanque1_nivel']<=30){
         $respuesta .= "0-";
-    }elseif ($consulta['tanque1_nivel']>80) {
+    }elseif ($consulta['tanque1_nivel']>=80) {
         $respuesta .= "1-";
+    }else{
+        $respuesta .= "2-";
     }
 
-    if ($consulta['tanque2_nivel']<30){
+    if ($consulta['tanque2_nivel']<=30){
         $respuesta .= "0-";
-    }elseif ($consulta['tanque2_nivel']>80) {
+    }elseif ($consulta['tanque2_nivel']>=80) {
         $respuesta .= "1-";
+    }else{
+        $respuesta .= "2-";
     }
 
-    if ($consulta['tanque3_nivel']<30){
+    if ($consulta['tanque3_nivel']<=30){
         $respuesta .= "0";
-    }elseif ($consulta['tanque3_nivel']>80) {
+    }elseif ($consulta['tanque3_nivel']>=80) {
         $respuesta .= "1";
+    }else{
+        $respuesta .= "2";
     }
 }
 
